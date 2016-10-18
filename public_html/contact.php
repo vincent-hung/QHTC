05a7339e64982889b09de532309e7281fa1a8337
SirLeonardIV <leonard.ha@gmail.com>
2016-10-18 18:58:24 -0400

    First push of QHTC site

---
+++ public_html/contact.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset = "UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content = "width=device-width, initial-scale = 1">
+<title>QHTC - Contact</title>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="QHTCcss.css"/>
+</head>
+
+<body>
+<div class="container-fluid">
+	<div class="row">
+	<div class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+
+				<div class="navbar-brand">
+                	<a href="home.php" class="navbutton">QHTC</a>
+                </div>
+
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+           		</button>
+
+            </div>
+                    
+            <div class="collapse navbar-collapse navHeaderCollapse">
+                <ul class="nav navbar-nav navbar-right">
+                   	<li><a href="about.php" class="navbutton" id="button1" >About</a></li>
+                    <li><a href="events.php" class="navbutton" id="button2">Events</a></li>
+                    <li><a href="resources.php" class="navbutton" id="button3">Resources</a></li>
+                    <li><a href="contact.php" class="navbutton" id="button4">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+	</div>
+	</div>
+
+	<div class="row content">
+		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 title">
+			Contact us
+			<hr class="hr">
+		</div>
+	</div>
+
+	<div class="row footer">
+
+	</div>
+
+	
+</div>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</html>
+
+<script>
+	$(document).ready(function(){
+
+	});
+</script>
